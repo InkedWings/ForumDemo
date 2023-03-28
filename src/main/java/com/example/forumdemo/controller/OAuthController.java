9b5424b96813579f9b99daa478bfce087f20fd2f
InkedWings <yezhijing110@outlook.com>
2023-03-28 16:57:40 -0400

    add check cookies

--- src/main/java/com/example/forumdemo/controller/OAuthController.java
+++ src/main/java/com/example/forumdemo/controller/OAuthController.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -33,7 +35,8 @@ public class OAuthController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -45,13 +48,14 @@ public class OAuthController {
         if(gitHubUser != null){
             // Log in successfully, create session and cookie
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(gitHubUser.getName());
             user.setAccoutId(String.valueOf(gitHubUser.getId()));
             user.setGmt_create(System.currentTimeMillis());
             user.setGmt_modified(System.currentTimeMillis());
             userMapper.insert(user);
-            request.getSession().setAttribute("user", gitHubUser);
+            response.addCookie(new Cookie("token", token));
             return "redirect:/";
         }else{
             // Log in failed
--- src/main/java/com/example/forumdemo/mapper/UserMapper.java
+++ src/main/java/com/example/forumdemo/mapper/UserMapper.java
@@ -3,9 +3,14 @@ package com.example.forumdemo.mapper;
 import com.example.forumdemo.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user_info (name, account_id, token, gmt_create, gmt_modified) values (#{name}, #{accountId}, #{token}, #{gmt_create}, #{gmt_modified})")
     void insert(User user);
+
+    @Select("select * from user_info where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
